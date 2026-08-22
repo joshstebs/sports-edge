ff1809f8f947f3ff61af1eab13cb1a7230f0e52e
joshstebs <josh.stebs@gmail.com>
2026-08-22 19:23:36 -0400

    Enable reconciled visual experience route

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
 import App from './App';
+import ExperienceApp from './ExperienceApp';
 import './index.css';
+
+const params = new URLSearchParams(window.location.search);
+const experienceMode = params.get('experiences') === '1';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {experienceMode ? <ExperienceApp /> : <App />}
     <Analytics />
   </StrictMode>,
 );
